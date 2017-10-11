97ded83dc6b68c5b4315d206c53ab645f457e138
jfong361 <jfong361@gmail.com>
2017-10-11 17:31:49 +1100

    remove redundant files

--- templates/foo.R
+++
@@ -1,8 +0,0 @@
-foo <- function(args) {
-    print(names(args))
-    for (key in names(args)) {
-        print(args[[key]])
-    }
-}
-df = read.csv("/Users/harry/AGRF/data/PCR1_B5HC6_CAACGACG-CGTGAT_L001/abundance/PCR1_L001_vjassoc.csv", stringsAsFactors=FALSE)
-foo(list("PCR1"="PCR1_L001_ACA", "PCR2"="WHAT", df="PCR1"))
