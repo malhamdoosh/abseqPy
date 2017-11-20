4f8d17a330f53f67705308d711ba716e89324456
jfong361 <jfong361@gmail.com>
2017-11-20 12:22:23 +1100

    productivity plot complete

--- templates/productivity.R
+++ templates/productivity.R
@@ -1,39 +1,51 @@
-#################################################################################
-#   Author : JiaHong FONG                                                       #
-#   Date   : Tue 26 Sep 2017 15:27:05 AEST                                      #
-#   Purpose: plots a facet_grid plot to show n-samples with productivity        #
-#            comparision (and reason - stop/frame/both)                         #
-#################################################################################
-
 library(ggplot2)
-library(ggrepel)
 
-productivty_plot <- function(sample_name1, sample_name2, sample_name3, dir1, dir2, dir3, path_to_clone) {
-
-    file1 <- paste(dir1, path_to_clone, sample_name1, "_productivity.csv", sep="")
-    file2 <- paste(dir2, path_to_clone, sample_name2, "_productivity.csv", sep="")
-    file3 <- paste(dir3, path_to_clone, sample_name3, "_productivity.csv", sep="")
-
-    df1 <- read.csv(file1)
-    df2 <- read.csv(file2)
-    df3 <- read.csv(file3)
-
-    df1$round <- rep(sample_name1, nrow(df1))
-    df2$round <- rep(sample_name2, nrow(df2))
-    df3$round <- rep(sample_name3, nrow(df3))
-
-    df.inter_union <- rbind(df1, df2)
-    df.union <- rbind(df.inter_union, df3)
-    drops <- c("X")
-    df.union <- df.union[, !(names(df.union) %in% drops)]
-
-    g <- ggplot(df.union, aes(round, Percentage)) +
-      geom_bar(stat="identity", aes(fill=Reason), width=0.5) +
-      facet_grid(~ Productivity)+
-      labs(title="Productivity",
-          subtitle="Percentage of unproductive reads due to stop codons and frameshifts",
-          x="Round",
-          y="Percentage") +
-      scale_y_continuous(limits=c(0,100))
-    plot(g)
+productivityPlot <- function(dataframes, sampleNames) {
+  # Plots a facet_grid histogram for all samples in dataframes.
+  #
+  # Args:
+  #     dataframes: A list() type. List of dataframes
+  #     sampleNames: A vector type. Vector of sampleNames corresponding 1-1 to dataframes
+  # Returns:
+  #     ggplot().
+  
+  nsamples <- length(dataframes)
+  # sanity check
+  stopifnot(length(sampleNames) == nsamples)
+  
+  #   ---- clean & pre-processing ----
+  unusedCols <- c("X")
+  # label each row with sample name and drop unused columns
+  for (i in 1:nsamples) {
+    df <- dataframes[[i]]
+    df$round <- rep(sampleNames[i], nrow(df))
+    dataframes[[i]] <- df[, !(names(df) %in% unusedCols)]
+  }
+  #   ---- done: clean & pre-processing ----
+  
+  # merge all samples
+  if (nsamples > 1) {
+    df.union <- rbind(dataframes[[1]], dataframes[[2]])
+    for (i in 3:nsamples) {
+      df.union <- rbind(df.union, dataframes[[i]])
+    }
+  } else {
+    df.union <- dataframes[[1]]
+  }
+  
+  # plot!
+  g <- ggplot(df.union, aes(round, Percentage, label = sprintf("%0.2f%%", round(Percentage)))) +
+    geom_bar(stat="identity", aes(fill=Reason), width=0.5) +
+    facet_grid(~ Productivity)+
+    labs(title="Productivity",
+         subtitle="Percentage of unproductive reads due to stop codons and frameshifts",
+         x="Round",
+         y="Percentage") +
+    scale_y_continuous(limits=c(0,100)) +
+    geom_text(position = position_stack(vjust = 0.5))
+  return (g)
 }
+# files <- list.files(pattern = "PCR[123].*_productivity.csv$", full.names = TRUE, recursive = TRUE)
+# dataframes <- lapply(files, read.csv, stringsAsFactors = FALSE)
+# p <- productivityPlot(dataframes, c("PCR1_L001", "PCR2_L001", "PCR3_L001"))
+# ggsave("/Users/harry/prod.pdf", plot = p)
