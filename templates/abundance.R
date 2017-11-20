6134f6c31e1c07539fc52574d1616ec0d85faa68
jfong361 <jfong361@gmail.com>
2017-11-20 11:52:07 +1100

    abundance plots complete

--- templates/abundance.R
+++ templates/abundance.R
@@ -1,131 +1,96 @@
-#################################################################################
-#   Author : JiaHong FONG                                                       #
-#   Date   : Tue 26 Sep 2017 15:27:05 AEST                                      #
-#   Purpose: plots a dodging bar plot of 3 samples (family V-D-J) distribution  #
-#################################################################################
-
 library(ggplot2)
-library(ggrepel)
-library(gridExtra)
-theme_set(theme_classic())
 
-plot_dist <- function(df1, df2, df3, family, sample_name1, sample_name2, sample_name3, title="family") {
+
+plotDist <- function(dataframes, sampleNames, gene, title="family") {
+  # Plots a dodging histogram for all sample in dataframes.
+  #
+  # Args:
+  #   dataframes: A list() type. List of dataframes
+  #   sampleNames: A vector type. Vector of sampleNames corresponding 1-1 to dataframes.
+  #   gene: A string type. The expression level of dataframe (either Family, Gene, Variant)
+  #   title: A string type. If it's anything other than family, there will be a capped imposed on
+  #          the number of x-axis values displayed (15 default)
+  # Returns:
+  #   ggplot().
+  
+  frames <- length(dataframes)
+  # sanity check
+  stopifnot(frames == length(sampleNames))
+  
+  # define maximum cutoff x-axis values to display
   CUTOFF <- 15
-  # ### CLEANUP #### #
-  # Don't need the total row (remove it)
-  df1 <- df1[!(df1$Germline.group == 'TOTAL'), ]
-  df2 <- df2[!(df2$Germline.group == 'TOTAL'), ]
-  df3 <- df3[!(df3$Germline.group == 'TOTAL'), ]
+  # If there was a cutoff, caps will display the right message, by default it's empty
   caps <- ""
   
+  # ------------  cleaning & transforming data   ----------- #
+  
+  for (i in 1:frames) {
+    
+    # remove the row that says 'TOTAL'
+    df <- dataframes[[i]]
+    df <- df[!(df$Germline.group == 'TOTAL'), ]
+    
+    # for each dataframe, append a new column 'sample' to indicate where this df came from
+    df$sample <- rep(sampleNames[i], nrow(df))
+    
+    # sort the values by descending order (strictly speaking,
+    # not necessary because it's already sorted - but better be safe than sorry)
+    dataframes[[i]] <- df[with(df, order(-Count)), ]
+  }
+  
+  
+  # check if cut-off needs to happen (if title == family, there is always < 15 families anyway)
   if (title != "family") {
-    if (nrow(df1) > CUTOFF) {
-      caps <- "Cutoff at top 15"
-      df1 <- head(df1, CUTOFF)
-    }
-    if (nrow(df2) > CUTOFF) {
-      caps <- "Cutoff at top 15"
-      df2 <- head(df2, CUTOFF)
-    }
-    if (nrow(df3) > CUTOFF) {
-      caps <- "Cutoff at top 15"
-      df3 <- head(df3, CUTOFF)
+    for (i in 1:frames) {
+      df <- dataframes[[i]]
+      if (nrow(df) > CUTOFF) {
+        caps <- "Cutoff at top 15"
+        dataframes[[i]] <- head(df, CUTOFF)
+      }
     }
   }
-  # ### CLEANUP DONE #### #
   
-  # sort descending order (not necessary, but sort anyway).
-  df1 <- df1[with(df1, order(-Count)), ]
-  df2 <- df2[with(df2, order(-Count)), ]
-  df3 <- df3[with(df3, order(-Count)), ]
+  # --------- complete: cleaning & transforming data -------- #
   
-  # add a column specifying where this data came from (which sample)
-  df1$round <- rep(sample_name1, nrow(df1))
-  df2$round <- rep(sample_name2, nrow(df2))
-  df3$round <- rep(sample_name3, nrow(df3))
   
-  df.intermediate_union <- rbind(df1, df2)
-  df.union <- rbind(df.intermediate_union, df3)
+  # merge dataframes into one
+  df.union <- rbind(dataframes[[1]], dataframes[[2]])
+  for (i in 3:frames) {
+    df <- dataframes[[i]]
+    df.union <- rbind(df.union, df)
+  }
   
   g <- ggplot(df.union, aes(Germline.group, Percentage....)) +
-    geom_bar(stat="identity", aes(fill=round), width=0.5, position="dodge") +
-    theme(text=element_text(size=10), axis.text.x = element_text(angle=74, vjust=0.4)) +
-    #geom_text_repel(aes(label = Count), size = 3) +
-    #geom_text(aes(label = Count), vjust=-1, size = 3) +
-    labs(title=paste("Histogram of ", family, " ", title, " distribution", sep=""),
-         subtitle=paste("Percentage of IGH", family, " ", title," distribution across all rounds", sep=""),
-         x=paste(family,"-Germline ", title , sep=""),
-         y="Percentage", caption=caps)
-  return(g)
-}
-
-# ====================================================== #
-# Family level stacked bar plot (Histogram)
-# ====================================================== #
-abundance_plot <- function(sample_name1, sample_name2, sample_name3, dir1, dir2, dir3, path_to_clone) {
-  # V-FAMILY
-  file1.v <- paste(dir1, path_to_clone, sample_name1, "_igv_dist_family_level.csv", sep="")
-  file2.v <- paste(dir2, path_to_clone, sample_name2, "_igv_dist_family_level.csv", sep="")
-  file3.v <- paste(dir3, path_to_clone, sample_name3, "_igv_dist_family_level.csv", sep="")
-  df1.v <- read.csv(file1.v, stringsAsFactors=FALSE)
-  df2.v <- read.csv(file2.v, stringsAsFactors=FALSE)
-  df3.v <- read.csv(file3.v, stringsAsFactors=FALSE)
-  
-  # D-FAMILY
-  file1.d <- paste(dir1, path_to_clone, sample_name1, "_igd_dist_family_level.csv", sep="")
-  file2.d <- paste(dir2, path_to_clone, sample_name2, "_igd_dist_family_level.csv", sep="")
-  file3.d <- paste(dir3, path_to_clone, sample_name3, "_igd_dist_family_level.csv", sep="")
-  df1.d <- read.csv(file1.d, stringsAsFactors=FALSE)
-  df2.d <- read.csv(file2.d, stringsAsFactors=FALSE)
-  df3.d <- read.csv(file3.d, stringsAsFactors=FALSE)
-  
-  # J-FAMILY
-  file1.j <- paste(dir1, path_to_clone, sample_name1, "_igj_dist_family_level.csv", sep="")
-  file2.j <- paste(dir2, path_to_clone, sample_name2, "_igj_dist_family_level.csv", sep="")
-  file3.j <- paste(dir3, path_to_clone, sample_name3, "_igj_dist_family_level.csv", sep="")
-  df1.j <- read.csv(file1.j, stringsAsFactors=FALSE)
-  df2.j <- read.csv(file2.j, stringsAsFactors=FALSE)
-  df3.j <- read.csv(file3.j, stringsAsFactors=FALSE)
-  
-  p1 <- plot_dist(df1.v, df2.v, df3.v, "V", sample_name1, sample_name2, sample_name3)
-  p2 <- plot_dist(df1.d, df2.d, df3.d, "D", sample_name1, sample_name2, sample_name3)
-  p3 <- plot_dist(df1.j, df2.j, df3.j, "J", sample_name1, sample_name2, sample_name3)
-  
-  # ====================================================== #
-  # Gene level stacked bar plot (Histogram)
-  # ====================================================== #
-  
-  # V-GENE
-  file1.v <- paste(dir1, path_to_clone, sample_name1, "_igv_dist_gene_level.csv", sep="")
-  file2.v <- paste(dir2, path_to_clone, sample_name2, "_igv_dist_gene_level.csv", sep="")
-  file3.v <- paste(dir3, path_to_clone, sample_name3, "_igv_dist_gene_level.csv", sep="")
-  df1.v <- read.csv(file1.v, stringsAsFactors=FALSE)
-  df2.v <- read.csv(file2.v, stringsAsFactors=FALSE)
-  df3.v <- read.csv(file3.v, stringsAsFactors=FALSE)
-  
-  # D-GENE
-  file1.d <- paste(dir1, path_to_clone, sample_name1, "_igd_dist_gene_level.csv", sep="")
-  file2.d <- paste(dir2, path_to_clone, sample_name2, "_igd_dist_gene_level.csv", sep="")
-  file3.d <- paste(dir3, path_to_clone, sample_name3, "_igd_dist_gene_level.csv", sep="")
-  df1.d <- read.csv(file1.d, stringsAsFactors=FALSE)
-  df2.d <- read.csv(file2.d, stringsAsFactors=FALSE)
-  df3.d <- read.csv(file3.d, stringsAsFactors=FALSE)
-  
-  # J-GENE
-  file1.j <- paste(dir1, path_to_clone, sample_name1, "_igj_dist_variant_level.csv", sep="")
-  file2.j <- paste(dir2, path_to_clone, sample_name2, "_igj_dist_variant_level.csv", sep="")
-  file3.j <- paste(dir3, path_to_clone, sample_name3, "_igj_dist_variant_level.csv", sep="")
-  df1.j <- read.csv(file1.j, stringsAsFactors=FALSE)
-  df2.j <- read.csv(file2.j, stringsAsFactors=FALSE)
-  df3.j <- read.csv(file3.j, stringsAsFactors=FALSE)
-  
-  p1.p <- plot_dist(df1.v, df2.v, df3.v, "V", sample_name1, sample_name2, sample_name3, "gene")
-  p2.p <- plot_dist(df1.d, df2.d, df3.d, "D", sample_name1, sample_name2, sample_name3, "gene")
-  ### REMEMBER, abudance_plot(J ==> title="variant" NOT GENE)
-  p3.p <- plot_dist(df1.j, df2.j, df3.j, "J", sample_name1, sample_name2, sample_name3, "variant")
-  
-  grid.arrange(p1, p2, p3, p1.p, p2.p, p3.p, ncol=3)
+    geom_bar(stat='identity', aes(fill = sample), width = 0.5, position = 'dodge') +
+    theme(text = element_text(size = 10), axis.text.x = element_text(angle = 74, vjust = 0.4)) +
+    labs(title = paste("Histogram of", gene, title, "distribution"),
+         subtitle = paste("Percentage of IGH", gene, " ", title, " distribution across all samples", sep = ""),
+         x = paste(gene, "-Germline ", title, sep = ""),
+         y = "Percentage",
+         caption = caps)
+  return (g)
 }
 
 
-abundance_plot(sample_name1, sample_name2, sample_name3, dir1, dir2, dir3, path_to_clone)
+# sampleNames can be obtained as such : names(data_fames) <- gsub("\\.csv$", "", my_files)
+abundancePlot <- function(fs, sampleNames) {
+  for (expression in c("family", "gene")) {
+    for (gene in c('v', 'd', 'j')) {
+      # correction, J has no "gene" but rather variant
+      if (gene == 'j' && expression == 'gene') {
+        expression <- 'variant'
+      }
+      reg <- paste(".*_ig", gene, "_dist_", expression, "_level.csv$", sep = "")
+      selectedFiles <- fs[grepl(reg, fs)]
+      dataframes <- lapply(selectedFiles, read.csv, stringsAsFactors=FALSE)
+      p <- plotDist(dataframes, sampleNames, toupper(gene), expression)
+      # ggsave(paste("/Users/harry/", expression, gene, debu, ".pdf", sep=""), plot = p)
+    }
+  }
+}
+
+# https://stackoverflow.com/questions/17499013/how-do-i-make-a-list-of-data-frames
+# test driver:
+# fs can be obtained as such fs <- list.files(pattern = "\\.csv$")
+#fs <- list.files(pattern = "PCR[123].*ig[vdj]_dist_[family|gene|variant].*\\.csv$", full.names = TRUE, recursive = TRUE)
+#abundancePlot(fs, c("PCR1", "PCR2", "PCR3"))
