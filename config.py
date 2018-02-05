af0401d765e61be8122a8c70b91e9657f014028f
jfong <jfong361@gmail.com>
2018-02-05 15:13:59 +1100

    version bump 1.1.3

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ import sys
 #SAMTOOLS_PROGRAM= 'samtools'
 #BGZIP = 'bgzip'
 #TABIX = 'tabix'
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 CLUSTALW = 'clustalw2'
 CLUSTALOMEGA = 'clustalo'
 WEBLOGO = 'weblogo'
