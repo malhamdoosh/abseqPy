1a8e98a2d2d4ea1bdc13c71e906103ef6abf7c63
jfong <jfong361@gmail.com>
2018-03-21 18:30:32 +1100

    Version 1.1.8

--- setup.py
+++ setup.py
@@ -442,7 +442,7 @@ def readme():
 
 
 setup(name="AbSeq",
-      version="1.1.7",
+      version="1.1.8",
       description="Quality control pipeline for antibody libraries",
       license="placeholder",
       long_description=readme(),
