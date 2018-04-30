7b423dc2fd351d12fd2171ca988b9b77c7e9ce3f
jfong <jfong361@gmail.com>
2018-04-30 16:06:11 +1000

    Add resource manager to multiprocessing

--- abseq/IgMultiRepertoire/AbSeqWorker.py
+++ abseq/IgMultiRepertoire/AbSeqWorker.py
@@ -1,7 +1,7 @@
 import traceback
 import sys
 
-from multiprocessing import Process, current_process
+from multiprocessing import Process, current_process, Lock, Value
 
 
 class AbSeqWorker(Process):
@@ -16,10 +16,11 @@ class AbSeqWorker(Process):
                                                                           'analyzePrimerSpecificity', \
                                                                           'analyzeSeqLen'
 
-    def __init__(self, repertoire, resultQueue):
+    def __init__(self, repertoire, resultQueue, resourcePool):
         super(AbSeqWorker, self).__init__()
         self.repertoire = repertoire
         self.resultQueue = resultQueue
+        self.resourcePool = resourcePool
 
     def run(self):
         while True:
@@ -29,9 +30,11 @@ class AbSeqWorker(Process):
                 # all jobs done for this repertoire
                 self.repertoire._minimize()
                 self.resultQueue.put(self.repertoire)
+                # return resource to available pool
+                self.resourcePool.increment(self.repertoire.threads)
                 break
-
             try:
+                self.repertoire.threads += self.resourcePool.consume()
                 getattr(self.repertoire, task)(*args, **kwargs)
             except Exception as e:
                 exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
@@ -50,3 +53,30 @@ class AbSeqWorkerException(Exception):
         super(AbSeqWorkerException, self).__init__(message)
         self.errors = errors
         self.tracebackMsg = ''.join(tracebackMsg)
+
+
+class ResourcePool:
+    # https://eli.thegreenplace.net/2012/01/04/shared-counter-with-pythons-multiprocessing
+    # beware multiprocessing.Value doesn't actually guarantee that 2 processes wont access the value at the same time,
+    # we need to warp it with a lock
+    def __init__(self, initResource):
+        self._lock = Lock()
+        self.n = Value('i', initResource)
+
+    def increment(self, n=1):
+        with self._lock:
+            self.n.value += n
+            assert self.n.value >= 0
+
+    def value(self):
+        with self._lock:
+            return self.n.value
+
+    def consume(self):
+        with self._lock:
+            if self.n.value > 0:
+                n = self.n.value
+                self.n.value = 0
+                return n
+            return 0
+
