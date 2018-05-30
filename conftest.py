14c614c7a20a939abcb522115bd35c337a78bd17
jfong <jfong361@gmail.com>
2018-05-30 18:35:55 +1000

    Update conftest

--- conftest.py
+++ conftest.py
@@ -6,3 +6,7 @@ import numpy
 def add_np(doctest_namespace):
     doctest_namespace['np'] = numpy
 
+
+@pytest.fixture(autouse=True)
+def add_raises(doctest_namespace):
+    doctest_namespace['raises'] = pytest.raises
